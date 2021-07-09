c2e83f413feb49a541d740142b12088a79516369
yangkaiqi <yangkaiqi191218@credithc.com>
2021-07-09 18:47:31 +0800

    整理

---
+++ basicsMyBatis/src/main/java/com/ykq/configuration/MyBatisConfiguration.java
@@ -0,0 +1,12 @@
+package com.ykq.configuration;
+
+/**
+ * @ClassName MyBatisConfiguration
+ * @Description TODO
+ * @Author ykq
+ * @Date 2021/7/9
+ * @Version v1.0.0
+ */
+
+public class MyBatisConfiguration {
+}
